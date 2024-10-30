eddbb1edb6b06d6b0c71120cc8b0c84ac8912cb9
gawmk <mikolaj.gawrys@gmail.com>
2024-10-30 14:29:53 +0100

    testing pdftools

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -485,3 +485,17 @@ or go back to just one window (by deleting all but the selected window)."
 (gawmk/leader-key
   "mg" '(magit-status :which-key "magit status pane")
   "cmg" '(magit-clone :which-key "clone a repository"))
+
+(use-package pdf-tools
+  :defer t
+  :commands (pdf-loader-install)
+  :mode "\\.pdf\\"
+  :bind (:map pdf-view-mode-map
+              ("j" . pdf-view-next-line-or-next-page)
+              ("k" . pdf-view-previous-line-or-next-page)
+              ("C-+" . pdf-view-enlarge)
+              ("C--" . pdf-view-shrink))
+  :init (pdf-loader-install)
+  :config (add-to-list 'revert-without-query ".pdf"))
+
+(add-hook 'pdf-view-mode-hook #'(lambda () (interactive) (display-line-numbers-mode -1)))
